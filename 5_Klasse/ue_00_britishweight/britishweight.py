5354192dca78ecd928069a1ce55e31fe55043b8a
Clemens Hodina <9183@htl.rennweg.at>
2023-09-11 16:30:59 +0200

    basic things like constructor, properties and __str__

--- 5_Klasse/ue_00_britishweight/britishweight.py
+++ 5_Klasse/ue_00_britishweight/britishweight.py
@@ -1,7 +1,26 @@
 #Clemens Hodina, 5CN
+from math import floor
+
 
 class Britishweight:
 
+    def __init__(self, stones, pounds):
+        self._pounds = pounds + (stones * 14)
+
+    def stones(self):
+        if self.pounds() >= 0:
+            return floor(self.pounds() / 14)
+        else:
+            return -1 * floor(-1 * self.pounds() / 14)
+
+    def pounds(self):
+        return self._pounds
+
+    def __str__(self):
+        return str(str(self.stones) + ' st ' + str(self.pounds) + ' lb')
+
+    def __repr__(self):
+        return str(str(self.stones) + ' st ' + str(self.pounds) + ' lb')
 
 
 
