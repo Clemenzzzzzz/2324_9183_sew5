9d5056208ddde509992d5dd8d53b63bffbd94213
Clemens Hodina <9183@htl.rennweg.at>
2023-09-11 17:05:31 +0200

    javadoc

--- src/ue_00_schaltung/FlipFlop.java
+++ src/ue_00_schaltung/FlipFlop.java
@@ -4,27 +4,41 @@
 
 package ue_00_schaltung;
 
-public class flipflop {
+public class FlipFlop {
     private boolean isTrue = false;
 
+    /**
+     * sets the State of the FlipFlop to true
+     */
     public void setState() {
         isTrue = true;
     }
 
+    /**
+     * resets the state of the FlipFLop to its default value
+     */
     public void resetState() {
         isTrue = false;
     }
 
+    /**
+     * returns the output of the q
+     * @return value of q output
+     */
     public boolean getQ () {
         return isTrue;
     }
 
+    /**
+     * returns the output of the inverted q
+     * @return value of inverted q output
+     */
     public boolean getInvertedQ() {
         return !isTrue;
     }
 
     public static void main(String[] args) {
-        flipflop f1 = new flipflop();
+        FlipFlop f1 = new FlipFlop();
 
         f1.setState();
         System.out.println(f1.getQ());
