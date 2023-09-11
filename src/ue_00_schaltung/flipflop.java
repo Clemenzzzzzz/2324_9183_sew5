257c5364779ac367c2c23d0a6a87288b2993db3b
Clemens Hodina <9183@htl.rennweg.at>
2023-09-11 16:59:05 +0200

    basic functions like setting, resetting, getting the value of q and getting the value of the inverted q

--- src/ue_00_schaltung/flipflop.java
+++ src/ue_00_schaltung/flipflop.java
@@ -5,4 +5,23 @@
 package ue_00_schaltung;
 
 public class flipflop {
+    private boolean isTrue = false;
+
+    public void setState() {
+        isTrue = true;
+    }
+
+    public void resetState() {
+        isTrue = false;
+    }
+
+    public boolean getQ () {
+        return isTrue;
+    }
+
+    public boolean getInvertedQ() {
+        return !isTrue;
+    }
+
+
 }
