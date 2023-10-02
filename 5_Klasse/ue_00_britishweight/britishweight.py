65810b73aea7ec9f219315840bef55087a457e71
Clemens Hodina <9183@htl.rennweg.at>
2023-10-02 17:37:19 +0200

    britishweight complete

--- 5_Klasse/ue_00_britishweight/britishweight.py
+++ 5_Klasse/ue_00_britishweight/britishweight.py
@@ -39,7 +39,7 @@ class Britishweight:
         return self._pounds % 14 - (14 if self._pounds < 0 and self._pounds % 14 != 0 else 0)
 
     def __str__(self):
-        return f'{str(int(self._pounds / 14)) + " st " if self._pounds > 14 else ""}' \
+        return f'{str(self._pounds // 14) + " st " if self._pounds > 14 else ""}' \
                f'{str(int(self._pounds % 14)) + " lb" if self._pounds % 14 != 0 else "0 lb" if self._pounds == 0 else ""}'
 
     def __repr__(self):
