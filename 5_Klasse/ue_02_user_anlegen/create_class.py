cf7e33ad6894612e037c011f12fc56025dc2f5e3
Clemens Hodina <9183@htl.rennweg.at>
2023-11-06 09:13:50 +0100

    python documentation

--- 5_Klasse/ue_02_user_anlegen/create_class.py
+++ 5_Klasse/ue_02_user_anlegen/create_class.py
@@ -8,8 +8,6 @@ import os.path
 from logging.handlers import RotatingFileHandler
 import logging
 
-
-
 logger = logging.getLogger(__name__)
 handler = logging.handlers.RotatingFileHandler("create_class.log", maxBytes=10000, backupCount=5)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -20,9 +18,6 @@ stream_handler.setFormatter(formatter)
 
 logger.addHandler(handler)
 logger.addHandler(stream_handler)
-
-
-
 
 """ Beispiel Excel auslesen 
 wb = load_workbook(xlsfilename, read_only=True)
@@ -37,7 +32,14 @@ ws['A1'] = 42 # Schreiben mit Zellenname
 user_to_password = []
 verbosity = False
 
+
 def read_class_excel(filename):
+    """
+        reads an excel file and gives back the values of each line
+
+        :param filename: file to read
+        :return: lines of the excel file as values
+        """
     lines = []
     wb = load_workbook(filename, read_only=True)
     ws = wb[wb.sheetnames[0]]
@@ -47,17 +49,18 @@ def read_class_excel(filename):
         lines.append((row[0].value, row[1].value, row[2].value))
     return lines
 
+
 def create_files(excel_file):
     """
-    erstellt die 4 Files und lässt sie von eigenen Methoden erstellen
+    creates and writes the required Files
 
-    :return:
+    :return: The required files at the place where the program is started
     """
     global user_to_password
     global verbosity
     global logger
     lines = read_class_excel(excel_file)
-    class_user_script = create_class_users_script(lines) #quasi ein String den man dann einfach in ein File schreibt
+    class_user_script = create_class_users_script(lines)  # quasi ein String den man dann einfach in ein File schreibt
     class_delete_script = create_class_delete_script()
     with open('create_class_script.sh', 'w') as f1:
         f1.write(class_user_script)
@@ -74,13 +77,16 @@ def create_files(excel_file):
         logger.info("user_password_list created")
         if verbosity:
             print("user_password_list created")
-    #print(line, 'ressources/class_user_script.txt')
-
-
-
+    # print(line, 'ressources/class_user_script.txt')
 
 
 def create_class_users_script(lines):
+    """
+        creates the actual bash script to build the required users
+
+        :param lines: lines of the excel file that need to be written as a script
+        :return: returns the script as a string so that it can be written into a file
+        """
     global user_to_password
     global verbosity
     global logger
@@ -107,9 +113,10 @@ def create_class_users_script(lines):
         username = username.replace('ß', 'ss')
         gecos_field = class_name + '_' + class_teacher
         home_directory = '/home/klassen/' + username
-        password = class_name + random.choice(random_chars) + room_number + random.choice(random_chars) + class_teacher + random.choice(random_chars)
+        password = class_name + random.choice(random_chars) + room_number + random.choice(
+            random_chars) + class_teacher + random.choice(random_chars)
         user_to_password.append((username, password))
-        #script += f'groupadd {username}\n'
+        # script += f'groupadd {username}\n'
         script += f'useradd -d {home_directory} -c \"{gecos_field}\" -m -g {username} -G {systemgroups} -s {home_shell} {username}\n'
         script += f'echo {username}:{password} | chpasswd\n'
         logger.info(f'User {username} with password created!')
@@ -120,6 +127,11 @@ def create_class_users_script(lines):
 
 
 def create_class_delete_script():
+    """
+        creates the delete script matching the create script
+
+        :return: the delete script as a string to be writeable
+        """
     global user_to_password
     global verbosity
     global logger
@@ -133,7 +145,13 @@ def create_class_delete_script():
             print('User {username} with password deleted!')
     return script
 
+
 def get_user_to_passwd_list():
+    """
+        writes a list for every user with its password
+
+        :return: the user-password list as a string
+        """
     global user_to_password
     global verbosity
     global logger
@@ -145,10 +163,16 @@ def get_user_to_passwd_list():
             print(f'User {user} and its password in user_to_password list')
     return list
 
-#create_files('ressources/Klassenraeume_2023.xlsx')
+
+# create_files('ressources/Klassenraeume_2023.xlsx')
 
 
 def main():
+    """
+        arg-parser for the program to be runnable
+
+        :return: -
+        """
     global verbosity
     global logger
     parser = argparse.ArgumentParser()
@@ -160,12 +184,12 @@ def main():
     args = parser.parse_args()
     logger.setLevel(logging.INFO)
     if args.verbose:
-        #global verbosity
+        # global verbosity
         verbosity = True
         print("Verbosity truned on")
 
     if args.quiet:
-        #global verbosity
+        # global verbosity
         verbosity = False
         print('Verbosity turned off')
 
--- 5_Klasse/ue_02_user_anlegen/create_user.py
+++ 5_Klasse/ue_02_user_anlegen/create_user.py
@@ -12,8 +12,6 @@ import os.path
 from logging.handlers import RotatingFileHandler
 import logging
 
-
-
 logger = logging.getLogger(__name__)
 handler = logging.handlers.RotatingFileHandler("create_class.log", maxBytes=10000, backupCount=5)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -24,10 +22,6 @@ stream_handler.setFormatter(formatter)
 
 logger.addHandler(handler)
 logger.addHandler(stream_handler)
-
-
-
-
 
 """ Beispiel Excel auslesen 
 wb = load_workbook(xlsfilename, read_only=True)
@@ -42,12 +36,14 @@ ws['A1'] = 42 # Schreiben mit Zellenname
 user_to_password = []
 existing_user_names = []
 verbosity = False
+
+
 def read_name_excel(filename):
     """
-    liest file ein und gibt es mit yield zeilenweise weiter
+    reads an excel file and gives back the values of each line
 
-    :param filename:
-    :return:
+    :param filename: file to read
+    :return: lines of the excel file as values
     """
     lines = []
     wb = load_workbook(filename, read_only=True)
@@ -55,22 +51,23 @@ def read_name_excel(filename):
     for row in ws.iter_rows(min_row=2):
         if row[0].value == None:
             break
-        #z = x.coordinate  # "Name" der Zelle
+        # z = x.coordinate  # "Name" der Zelle
         lines.append((row[0].value, row[1].value, row[2].value, row[3].value))
-    #ws['A1'] = 42  # Schreiben mit Zellenname
+    # ws['A1'] = 42  # Schreiben mit Zellenname
     return lines
+
 
 def create_files(excel_file):
     """
-    erstellt die 4 Files und lässt sie von eigenen Methoden erstellen
+    creates and writes the required Files
 
-    :return:
+    :return: The required files at the place where the program is started
     """
     global user_to_password
     global verbosity
     global logger
     lines = read_name_excel(excel_file)
-    real_user_script = create_real_users_script(lines) #quasi ein String den man dann einfach in ein File schreibt
+    real_user_script = create_real_users_script(lines)  # quasi ein String den man dann einfach in ein File schreibt
     real_delete_script = create_real_delete_script()
     with open('create_user_script.sh', 'w', encoding='utf-8') as f1:
         print(real_user_script)
@@ -88,13 +85,16 @@ def create_files(excel_file):
         logger.info("user_password_list created")
         if verbosity:
             print("user_password_list created")
-    #print(line, 'ressources/class_user_script.txt')
-
-
-
+    # print(line, 'ressources/class_user_script.txt')
 
 
 def create_real_users_script(lines):
+    """
+    creates the actual bash script to build the required users
+
+    :param lines: lines of the excel file that need to be written as a script
+    :return: returns the script as a string so that it can be written into a file
+    """
     global user_to_password
     global verbosity
     global logger
@@ -123,9 +123,10 @@ def create_real_users_script(lines):
             existing_user_names.append(username)
         gecos_field = firstname + '_' + lastname
         home_directory = '/home/' + username
-        password = username + random.choice(random_chars) + group + random.choice(random_chars) + user_class + random.choice(random_chars)
+        password = username + random.choice(random_chars) + group + random.choice(
+            random_chars) + user_class + random.choice(random_chars)
         user_to_password.append((username, password))
-        #script += f'groupadd {username}\n'
+        # script += f'groupadd {username}\n'
         script += f'useradd -d {home_directory} -c \"{gecos_field}\" -m -g {username} -G {systemgroups},{group} -s {home_shell} {username}\n'
         script += f'echo {username}:{password} | chpasswd\n'
         logger.info(f'User {username} with password created!')
@@ -136,16 +137,23 @@ def create_real_users_script(lines):
 
 
 def get_valid_username(lastname):
+    """
+    changes a lastname to a valid username in the script
+
+    :param lastname: the lastname as given in the excel file
+    :return: a valid username as string
+    """
     username = lastname.lower()
     username = shave_marks(username)
-    #username = unidecode.unidecode(username)
+    # username = unidecode.unidecode(username)
     username = username.replace(' ', '_')
-    username = username.replace('ä', 'ae') #TODO weirde Charactere (siehe letzten beiden Namen) --> vor allem der bug grade
+    username = username.replace('ä', 'ae')
     username = username.replace('ö', 'oe')
     username = username.replace('ü', 'ue')
     username = username.replace('ß', 'ss')
     username = re.sub(r'[^a-z0-9_]', '', username)
     return username
+
 
 def shave_marks(txt):
     """Remove all diacritic marks"""
@@ -157,6 +165,11 @@ def shave_marks(txt):
 # TODO tests siehe angbabe
 
 def create_real_delete_script():
+    """
+    creates the delete script matching the create script
+
+    :return: the delete script as a string to be writeable
+    """
     global user_to_password
     global verbosity
     global logger
@@ -170,7 +183,13 @@ def create_real_delete_script():
             print('User {username} with password deleted!')
     return script
 
-def get_user_to_passwd_list(): #TODO das auch als excel
+
+def get_user_to_passwd_list():  # TODO das auch als excel
+    """
+    writes a list for every user with its password
+
+    :return: the user-password list as a string
+    """
     global user_to_password
     global verbosity
     global logger
@@ -182,10 +201,16 @@ def get_user_to_passwd_list(): #TODO das auch als excel
             print(f'User {user} and its password in user_to_password list')
     return list
 
-#create_files('ressources/Klassenraeume_2023.xlsx')
+
+# create_files('ressources/Klassenraeume_2023.xlsx')
 
 
 def main():
+    """
+    arg-parser for the program to be runnable
+
+    :return: -
+    """
     global verbosity
     global logger
     parser = argparse.ArgumentParser()
@@ -197,12 +222,12 @@ def main():
     args = parser.parse_args()
     logger.setLevel(logging.INFO)
     if args.verbose:
-        #global verbosity
+        # global verbosity
         verbosity = True
         print("Verbosity truned on")
 
     if args.quiet:
-        #global verbosity
+        # global verbosity
         verbosity = False
         print('Verbosity turned off')
 
