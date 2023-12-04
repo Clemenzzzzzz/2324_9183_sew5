b7352638ffddec61062397ca1dfffb4523f2392d
Clemens Hodina <9183@htl.rennweg.at>
2023-12-04 19:33:53 +0100

    suchen_alle in python done

--- 5_Klasse_python/ue_02_labyrinth/Labyrinth.py
+++ 5_Klasse_python/ue_02_labyrinth/Labyrinth.py
@@ -20,8 +20,8 @@ def suchen(zeile, spalte, lab):
         return True
     lab = [list(row) for row in lab]
     lab[zeile][spalte] = 'X'
-    result_list = ["".join(row) for row in lab]
-    print_lab(result_list)
+    #result_list = ["".join(row) for row in lab]
+    #print_lab(result_list)
     if lab[zeile + 1][spalte] != '#' and lab[zeile + 1][spalte] != 'X':
         if suchen(zeile + 1, spalte, lab):
             return True
@@ -37,16 +37,52 @@ def suchen(zeile, spalte, lab):
     if lab[zeile][spalte - 1] != '#' and lab[zeile][spalte - 1] != 'X':
         if suchen(zeile, spalte - 1, lab):
             return True
-
     return False
 
+
+def suchen_alle(zeile, spalte, lab, counter):
+    if lab[zeile][spalte] == 'A':
+        return counter + 1
+
+    lab = [list(row) for row in lab]
+    lab[zeile][spalte] = 'X'
+    #result_list = ["".join(row) for row in lab]
+    #print_lab(result_list)
+
+    if lab[zeile + 1][spalte] != '#' and lab[zeile + 1][spalte] != 'X':
+        counter = suchen_alle(zeile + 1, spalte, lab, counter)
+
+    if lab[zeile][spalte + 1] != '#' and lab[zeile][spalte + 1] != 'X':
+        counter = suchen_alle(zeile, spalte + 1, lab, counter)
+
+
+    if lab[zeile - 1][spalte] != '#' and lab[zeile - 1][spalte] != 'X':
+        counter = suchen_alle(zeile - 1, spalte, lab, counter)
+
+
+    if lab[zeile][spalte - 1] != '#' and lab[zeile][spalte - 1] != 'X':
+        counter = suchen_alle(zeile, spalte - 1, lab, counter)
+    lab = [list(row) for row in lab]
+    lab[zeile][spalte] = ' '
+    return counter
+
+
+# TODO's:
+#   Zeitmessung und ausgabe
+#   argparse
+#   docstring
+#   alles nochmal gescheit testen und mit java vergleichen
+
+
+
 def main():
-    lab = read_lab('C:\\Schule\\5_Klasse\\SEW\python\\5_Klasse_python\\ue_02_labyrinth\\l1.txt')
+    lab = read_lab('C:\\Schule\\5_Klasse\\SEW\python\\5_Klasse_python\\ue_02_labyrinth\\l3.txt')
     #for line in lab:
     #    print(line[0])
     print(lab[11][6]) # --> that is the A
     print_lab(lab)
     print(suchen(1,1, lab))
+    print(suchen_alle(1, 1, lab, 0))
 
 
 if __name__ == "__main__":
