f0007a9af4e9428024a9571a4cf310fbba833de3
Clemens Hodina <9183@htl.rennweg.at>
2023-12-04 19:26:28 +0100

    suchen done

--- 5_Klasse_python/ue_02_labyrinth/Labyrinth.py
+++ 5_Klasse_python/ue_02_labyrinth/Labyrinth.py
@@ -15,6 +15,30 @@ def print_lab(lab):
     for line in lab:
         print(line)
 
+def suchen(zeile, spalte, lab):
+    if lab[zeile][spalte] == 'A':
+        return True
+    lab = [list(row) for row in lab]
+    lab[zeile][spalte] = 'X'
+    result_list = ["".join(row) for row in lab]
+    print_lab(result_list)
+    if lab[zeile + 1][spalte] != '#' and lab[zeile + 1][spalte] != 'X':
+        if suchen(zeile + 1, spalte, lab):
+            return True
+
+    if lab[zeile][spalte + 1] != '#' and lab[zeile][spalte + 1] != 'X':
+        if suchen(zeile, spalte + 1, lab):
+            return True
+
+    if lab[zeile - 1][spalte] != '#' and lab[zeile - 1][spalte] != 'X':
+        if suchen(zeile - 1, spalte, lab):
+            return True
+
+    if lab[zeile][spalte - 1] != '#' and lab[zeile][spalte - 1] != 'X':
+        if suchen(zeile, spalte - 1, lab):
+            return True
+
+    return False
 
 def main():
     lab = read_lab('C:\\Schule\\5_Klasse\\SEW\python\\5_Klasse_python\\ue_02_labyrinth\\l1.txt')
@@ -22,6 +46,7 @@ def main():
     #    print(line[0])
     print(lab[11][6]) # --> that is the A
     print_lab(lab)
+    print(suchen(1,1, lab))
 
 
 if __name__ == "__main__":
