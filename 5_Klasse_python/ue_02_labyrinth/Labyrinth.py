a41387fcb01d3ab17c1468873cf606b2597a6ab2
Clemens Hodina <9183@htl.rennweg.at>
2023-12-04 19:12:57 +0100

    laby can now be printed

--- 5_Klasse_python/ue_02_labyrinth/Labyrinth.py
+++ 5_Klasse_python/ue_02_labyrinth/Labyrinth.py
@@ -11,12 +11,17 @@ def read_lab(file_path):
     return lab
 
 
+def print_lab(lab):
+    for line in lab:
+        print(line)
 
 
 def main():
     lab = read_lab('C:\\Schule\\5_Klasse\\SEW\python\\5_Klasse_python\\ue_02_labyrinth\\l1.txt')
-    for line in lab:
-        print(line)
+    #for line in lab:
+    #    print(line[0])
+    print(lab[11][6]) # --> that is the A
+    print_lab(lab)
 
 
 if __name__ == "__main__":
