65b6f16350816f369967c0e401d675c234f9f232
Clemens Hodina <9183@htl.rennweg.at>
2024-01-22 12:07:14 +0100

    istall matplotlib and basics

---
+++ 5_Klasse_python/ue3_plot/plot1.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import math
+
+
+PI = math.pi # für besonders Tippfaule ;-)
+CNT = 1024
+intervall = (math.pi*2 / 1024)
+
+#X = [ ... for i in range ... ] # CNT Werte von -pi ... +pi
+#C = [math.cos(x) for x in X] # CNT cosinuswerte für x von -pi ... +pi
+#S = [math.sin(x) for x in X]
+
+
+
+
+
+
+
+
