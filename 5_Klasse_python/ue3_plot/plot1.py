03882783ef9f12198abd5b1c5c0574fbb2d1c230
Clemens Hodina <9183@htl.rennweg.at>
2024-01-22 17:02:23 +0100

    andere Linienfarbe

--- 5_Klasse_python/ue3_plot/plot1.py
+++ 5_Klasse_python/ue3_plot/plot1.py
@@ -11,8 +11,8 @@ C = [math.cos(x) for x in X] # CNT cosinuswerte für x von -pi ... +pi
 S = [math.sin(x) for x in X]
 
 plt.figure(figsize=(10,6), dpi=80)
-plt.plot(X, C, color="blue", linewidth=2.5, linestyle="-")
-plt.plot(X, S, color="red", linewidth=2.5, linestyle="-")
+plt.plot(X, C, color="green", linewidth=2.5, linestyle="-")
+plt.plot(X, S, color="orange", linewidth=2.5, linestyle="-")
 
 plt.xlim(min(X)*1.1, max(X)*1.1)
 plt.ylim(min(C)*1.1, max(C)*1.1)
@@ -26,7 +26,7 @@ plt.yticks([-1, 0, +1],
 [r'$-1$', r'$0$', r'$+1$'])
 
 
-plt.plot(X, S, color="red", linewidth=2.5, linestyle="-", label="sine")
+plt.plot(X, S, color="orange", linewidth=2.5, linestyle="-", label="sine")
 plt.legend(loc='upper left', frameon=False)
 
 ax = plt.gca()
@@ -38,14 +38,14 @@ ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data',0))
 
 t = 2*PI/3
-plt.plot([t,t],[0,math.cos(t)], color ='blue', linewidth=2.5, linestyle="--")
-plt.scatter([t,],[math.cos(t),], 50, color ='blue')
+plt.plot([t,t],[0,math.cos(t)], color ='green', linewidth=2.5, linestyle="--")
+plt.scatter([t,],[math.cos(t),], 50, color ='green')
 plt.annotate(r'$\sin(\frac{2\pi}{3})=\frac{\sqrt{3}}{2}$',
 xy=(t, math.sin(t)), xycoords='data',
 xytext=(+10, +30), textcoords='offset points', fontsize=16,
 arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
-plt.plot([t,t],[0,math.sin(t)], color ='red', linewidth=2.5, linestyle="--")
-plt.scatter([t,],[math.sin(t),], 50, color ='red')
+plt.plot([t,t],[0,math.sin(t)], color ='orange', linewidth=2.5, linestyle="--")
+plt.scatter([t,],[math.sin(t),], 50, color ='orange')
 plt.annotate(r'$\cos(\frac{2\pi}{3})=-\frac{1}{2}$',
 xy=(t, math.cos(t)), xycoords='data',
 xytext=(-90, -50), textcoords='offset points', fontsize=16,
