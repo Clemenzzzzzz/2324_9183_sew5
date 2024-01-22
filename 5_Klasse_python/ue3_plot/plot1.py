80946bd3514dbc0d71360528e80c1902bfb87367
Clemens Hodina <9183@htl.rennweg.at>
2024-01-22 16:46:06 +0100

    erste Graphik fertig

--- 5_Klasse_python/ue3_plot/plot1.py
+++ 5_Klasse_python/ue3_plot/plot1.py
@@ -6,11 +6,16 @@ PI = math.pi # für besonders Tippfaule ;-)
 CNT = 1024
 intervall = (math.pi*2 / 1024)
 
-#X = [ ... for i in range ... ] # CNT Werte von -pi ... +pi
-#C = [math.cos(x) for x in X] # CNT cosinuswerte für x von -pi ... +pi
-#S = [math.sin(x) for x in X]
+X = [ -PI + i * intervall for i in range(CNT) ] # CNT Werte von -pi ... +pi
+C = [math.cos(x) for x in X] # CNT cosinuswerte für x von -pi ... +pi
+S = [math.sin(x) for x in X]
 
+plt.figure(figsize=(10,6), dpi=80)
+plt.plot(X, C, color="blue", linewidth=2.5, linestyle="-")
+plt.plot(X, S, color="red", linewidth=2.5, linestyle="-")
 
+plt.savefig("plot1_hodina.png",dpi=72)
+plt.show() # Anzeigen: Danach kann man die Grafik nicht mehr ändern!
 
 
 
