8d3ee038f0a84bb6af0991962bc7e2ed16257c56
Clemens Hodina <9183@htl.rennweg.at>
2024-02-19 15:16:21 +0100

    git log methode fertig

--- 5_Klasse_python/ue3_plot/statistik.py
+++ 5_Klasse_python/ue3_plot/statistik.py
@@ -1,13 +1,26 @@
-from subprocess import Popen, PIPE
+#from subprocess import Popen, PIPE
 
-GIT_COMMIT_FIELDS = ['id', 'author_name', 'author_email', 'date', 'message']
-GIT_LOG_FORMAT = ['%H', '%an', '%ae', '%ad', '%s']
-GIT_LOG_FORMAT = '%x1f'.join(GIT_LOG_FORMAT) + '%x1e'
-p = Popen('git log --format="%s"' % GIT_LOG_FORMAT, shell=True, stdout=PIPE)
-(log, _) = p.communicate()
-log = log.strip('\n\x1e').split("\x1e")
-log = [row.strip().split("\x1f") for row in log]
-log = [dict(zip(GIT_COMMIT_FIELDS, row)) for row in log]
+#GIT_COMMIT_FIELDS = ['id', 'author_name', 'author_email', 'date', 'message']
+#GIT_LOG_FORMAT = ['%H', '%an', '%ae', '%ad', '%s']
+#GIT_LOG_FORMAT = '%x1f'.join(GIT_LOG_FORMAT) + '%x1e'
+#p = Popen('git log --format="%s"' % GIT_LOG_FORMAT, shell=True, stdout=PIPE)
+#(log, _) = p.communicate()
+#log = log.strip('\n\x1e').split("\x1e")
+#log = [row.strip().split("\x1f") for row in log]
+#log = [dict(zip(GIT_COMMIT_FIELDS, row)) for row in log]
+
+import subprocess
+import datetime
+import matplotlib.pyplot as plt
+
+def parse_git_log(git_folder):
+    command = ['git', 'log', '--pretty=format:%ad', '--date=iso']
+    process = subprocess.Popen(command, cwd=git_folder, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, universal_newlines=True)
+    output, _ = process.communicate()
+    commits = output.strip().split('\n')
+    commit_times = [datetime.datetime.fromisoformat(commit) for commit in commits]
+    return commit_times
+
 
 
 
