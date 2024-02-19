5f0c975e067b80d71a030f5e4a90007143750e8b
Clemens Hodina <9183@htl.rennweg.at>
2024-02-19 15:31:39 +0100

    method that creats plot done

--- 5_Klasse_python/ue3_plot/statistik.py
+++ 5_Klasse_python/ue3_plot/statistik.py
@@ -21,6 +21,32 @@ def parse_git_log(git_folder):
     commit_times = [datetime.datetime.fromisoformat(commit) for commit in commits]
     return commit_times
 
+def plot_commit_activity(commit_times):
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    weekday_counts = [0] * 7
+    hour_counts = [0] * 24
+
+    for commit_time in commit_times:
+        weekday_counts[commit_time.weekday()] += 1
+        hour_counts[commit_time.hour] += 1
+
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+    title = 'Clemens Hodina, Commits: ' + str(weekday_counts.count())
+    fig.suptitle(title)
+    axes[0].scatter(weekdays, weekday_counts)
+    axes[0].set_title('Commits nach Wochentag')
+    axes[0].set_xlabel('Wochentag')
+    axes[0].set_ylabel('Commits')
+
+    axes[1].scatter(range(24), hour_counts)
+    axes[1].set_title('Commits nach Uhrzeit')
+    axes[1].set_xlabel('Stunde')
+    axes[1].set_ylabel('Commits')
+
+    plt.tight_layout()
+    plt.savefig('git_stats_hodina.png')
+    plt.show()
+
 
 
 
