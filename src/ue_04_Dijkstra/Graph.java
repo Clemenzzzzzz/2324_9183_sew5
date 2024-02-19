922f36d6a1a12f03bd048320901d73946ed705da
Clemens Hodina <9183@htl.rennweg.at>
2024-02-19 17:10:18 +0100

    start od graph

--- src/ue_04_Dijkstra/Graph.java
+++ src/ue_04_Dijkstra/Graph.java
@@ -1,14 +1,32 @@
 package ue_04_Dijkstra;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.List;
 
-public class Graph {
+public class Graph{
     List<Node> nodes;
 
-    //TODO PQ
+    Comparator<Node> pq;
+    //PQ --> das müsste treeset sein, Schlüssel ist Name der Node, Value ist Distanz
+    // dann kann ich die priority queue immer überschreiben weil ja immer nur ein Schlüssel ist
 
     public void readGraphFromAdjacencyMatrixFile(Path file) {
+        StringBuilder data = new StringBuilder();
 
+        try (BufferedReader br = new BufferedReader(new FileReader(String.valueOf(file)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Append each line to the StringBuilder
+                data.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(data);
     }
 
     public String getAllPaths() {
