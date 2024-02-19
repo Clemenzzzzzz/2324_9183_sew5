f4ca3ae17392bfe25d48dd2c4b54afb43ed18a33
Clemens Hodina <9183@htl.rennweg.at>
2024-02-19 16:47:00 +0100

    structure done

--- src/ue_04_Dijkstra/Edge.java
+++ src/ue_04_Dijkstra/Edge.java
@@ -1,4 +1,8 @@
 package ue_04_Dijkstra;
 
 public class Edge {
+    private int distance;
+
+    private Node neighbour;
+
 }
--- src/ue_04_Dijkstra/Node.java
+++ src/ue_04_Dijkstra/Node.java
@@ -1,4 +1,47 @@
 package ue_04_Dijkstra;
 
+import java.util.TreeSet;
+
 public class Node {
+
+    String id;
+
+    TreeSet<Edge> Edges;
+
+    int distance;
+
+    Node previous;
+
+    boolean isVisited;
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "id='" + id + '\'' +
+                ", Edges=" + Edges +
+                ", distance=" + distance +
+                ", previous=" + previous +
+                ", isVisited=" + isVisited +
+                '}';
+    }
+
+    public String getPath() {
+        return null;
+    }
+
+    public void addEdge(){
+
+    }
+
+    public void init() {
+
+    }
+
+
+    //TODO maybe setStartNode
+
+
+    public void visit() {
+
+    }
 }
